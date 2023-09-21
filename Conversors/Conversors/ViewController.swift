45962e727420f969170184a3b1b12b8653556926
Luan <luanmarcosdev@gmail.com>
2023-09-21 15:14:49 -0300

    first commit Conversors

---
+++ Conversors/Conversors/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Conversors
+//
+//  Created by Luan Arruda on 21/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var tfValue: UITextField!
+    @IBOutlet var btUnit1: UIButton!
+    @IBOutlet var btUnit2: UIButton!
+    @IBOutlet var lbResult: UILabel!
+    @IBOutlet var lbResultUnit: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func showNext(_ sender: UIButton) {
+    }
+    
+    @IBAction func convert(_ sender: UIButton) {
+    }
+}
+
