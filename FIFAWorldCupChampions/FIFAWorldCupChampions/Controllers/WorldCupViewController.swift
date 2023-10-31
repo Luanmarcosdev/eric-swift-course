dd4e494194e8406263b6f01ac0516564b002f4ca
Luan <luanmarcosdev@gmail.com>
2023-10-31 10:55:48 -0300

    final project FIFA World Cup Champios

--- FIFAWorldCupChampions/FIFAWorldCupChampions/Controllers/WorldCupViewController.swift
+++ FIFAWorldCupChampions/FIFAWorldCupChampions/Controllers/WorldCupViewController.swift
@@ -11,14 +11,14 @@ class WorldCupViewController: UIViewController {
 
     var worldCup: WorldCup!
     
-    
     @IBOutlet weak var ivWinner: UIImageView!
     @IBOutlet weak var ivVice: UIImageView!
     @IBOutlet weak var lbScore: UILabel!
     @IBOutlet weak var lbWinner: UILabel!
     @IBOutlet weak var lbVice: UILabel!
-    
-    
+    @IBOutlet weak var tableView: UITableView!
+        
+
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "WorldCup \(worldCup.year)"
@@ -29,4 +29,36 @@ class WorldCupViewController: UIViewController {
         lbVice.text = worldCup.vice
     }
     
+}
+
+extension WorldCupViewController: UITableViewDelegate {
+    
+}
+
+extension WorldCupViewController: UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return worldCup.matches.count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let games = worldCup.matches[section].games
+        return games.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! GamesTableViewCell
+        
+        let match = worldCup.matches[indexPath.section]
+        let game = match.games[indexPath.row]
+        
+        cell.prepare(with: game)
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        let match = worldCup.matches[section]
+        return match.stage
+    }
 }
