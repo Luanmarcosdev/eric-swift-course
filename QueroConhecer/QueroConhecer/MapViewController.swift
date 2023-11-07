67d3fcc57ce5b79299414d2701625662eecd1a7f
Luan <luanmarcosdev@gmail.com>
2023-11-06 21:45:34 -0300

    adding project QueroConhecer

---
+++ QueroConhecer/QueroConhecer/MapViewController.swift
@@ -0,0 +1,119 @@
+//
+//  MapViewController.swift
+//  QueroConhecer
+//
+//  Created by Luan Arruda on 01/11/23.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+    
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var viInfo: UIView!
+    @IBOutlet weak var lbName: UILabel!
+    @IBOutlet weak var lbAddress: UILabel!
+    @IBOutlet weak var loading: UIActivityIndicatorView!
+    
+    var places: [Place]!
+    var poi: [MKAnnotation] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        searchBar.isHidden = true
+        viInfo.isHidden = true
+        mapView.delegate = self
+        
+        if places.count == 1 {
+            title = places[0].name
+        }else {
+            title = "Meus Lugares"
+        }
+        
+        for place in places {
+            addToMap(place)
+        }
+        
+        showPlaces()
+    }
+    
+    func showPlaces() {
+        mapView.showAnnotations(mapView.annotations, animated: true)
+    }
+    
+    func addToMap(_ place: Place){
+        let annotation = PlaceAnnotation(coordinate: place.coordinate, type: .place)
+        annotation.title = place.name
+        annotation.address = place.address
+        mapView.addAnnotation(annotation)
+    }
+    
+    @IBAction func showRoute(_ sender: UIButton) {
+    }
+    
+    @IBAction func showSearchBar(_ sender: UIBarButtonItem) {
+        searchBar.resignFirstResponder()
+        searchBar.isHidden = !searchBar.isHidden
+    }
+    
+    
+}
+
+
+extension MapViewController: MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        if !(annotation is PlaceAnnotation) {
+            return nil
+        }
+        let type = (annotation as! PlaceAnnotation).type
+        let identifier = "\(type)"
+        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier) as? MKMarkerAnnotationView
+        if annotationView == nil {
+            annotationView = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+        }
+        annotationView?.annotation = annotation
+        annotationView?.canShowCallout = true
+        annotationView?.markerTintColor = type == .place ? UIColor(named: "main") : UIColor(named: "poi")
+        annotationView?.glyphImage = type == .place ? UIImage(named: "placeGlyph") : UIImage(named: "poiGlyph")
+        annotationView?.displayPriority = type == .place ? .required : .defaultHigh
+        
+        return annotationView
+    }
+}
+
+extension MapViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.isHidden = true
+        searchBar.resignFirstResponder()
+        loading.startAnimating()
+        
+        let request = MKLocalSearch.Request()
+        request.naturalLanguageQuery = searchBar.text
+        request.region = mapView.region
+        let search = MKLocalSearch(request: request)
+        search.start { (response, error) in
+            if error == nil {
+                guard let response = response else {
+                    self.loading.stopAnimating()
+                    return
+                }
+                self.mapView.removeAnnotations(self.poi)
+                self.poi.removeAll()
+                for item in response.mapItems {
+                    let annotation = PlaceAnnotation(coordinate: item.placemark.coordinate, type: .poi)
+                    annotation.title = item.name
+                    annotation.subtitle = item.phoneNumber
+                    annotation.address = Place.getFormattedAddress(with: item.placemark)
+                    self.poi.append(annotation)
+                }
+                self.mapView.addAnnotations(self.poi)
+                self.mapView.showAnnotations(self.poi, animated: true)
+            }
+            self.loading.stopAnimating()
+        }
+        
+    }
+}
