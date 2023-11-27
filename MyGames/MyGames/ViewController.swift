1f863bd5deefb5c7bbbc1674062c79c83877b8ae
Luan <luanmarcosdev@gmail.com>
2023-11-27 12:18:33 -0300

    create project MyGames

---
+++ MyGames/MyGames/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyGames
+//
+//  Created by Luan Arruda on 27/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
