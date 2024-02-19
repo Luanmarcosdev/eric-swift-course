dc464a724062cd13e17dfccacedbee980d7db228
Luan <luanmarcosdev@gmail.com>
2024-02-19 14:42:02 -0300

    import CoreMotion

--- skullibrista/skullibrista/ViewController.swift
+++ skullibrista/skullibrista/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import CoreMotion
 
 class ViewController: UIViewController {
 
@@ -15,9 +16,46 @@ class ViewController: UIViewController {
     @IBOutlet weak var lbTimePlayed: UILabel!
     @IBOutlet weak var lbInstructions: UILabel!
     
+    var isMoving = false
+    lazy var motionManager = CMMotionManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         viGameOver.isHidden = true
+        
+        street.frame.size.width = view.frame.size.width * 2
+        street.frame.size.height = view.frame.size.height * 2
+        street.center = view.center
+        
+        player.center = view.center
+        player.animationImages = []
+        for i in 0...7 {
+            let image = UIImage(named: "player\(i)")!
+            player.animationImages?.append(image)
+        }
+        player.animationDuration = 0.5
+        player.startAnimating()
+        Timer.scheduledTimer(withTimeInterval: 6.0, repeats: false) { (time) in
+            self.start()
+        }
+    }
+    
+    func start(){
+        lbInstructions.isHidden = true
+        viGameOver.isHidden = true
+        isMoving = false
+        
+        if motionManager.isDeviceMotionAvailable {
+            motionManager.startDeviceMotionUpdates(to: OperationQueue.main) { (data, error) in
+                if error == nil {
+                    if let data = data {
+                        print("X:\(data.gravity.x), Y:\(data.gravity.y), Z:\(data.gravity.z)")
+                        let angle = atan2(data.gravity.x, data.gravity.y) - .pi
+                        self.player.transform = CGAffineTransform(rotationAngle: angle)
+                    }
+                }
+            }
+        }
     }
 
     @IBAction func playAgain(_ sender: Any) {
